ea1435d8aa43fbdb80d697c2e5cad35bbf5ca68b
dean-sharp <46000985+dean-sharp@users.noreply.github.com>
2019-04-14 11:21:45 +0100

    End of module one

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
+    new Recipe('A test recipe 2', 'This is simply a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+  ];
 
   constructor() { }
 
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -6,6 +6,6 @@ export class Recipe {
     constructor(name: string, description: string, imagePath: string) {
         this.name = name;
         this.description = description;
-        this,imagePath = imagePath;
+        this.imagePath = imagePath;
     }
 }
